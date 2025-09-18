7c0bcaebe3538290d85a0ee448e6c0d5d6c3a96a
TIme0MAL <zaouihamza263@gmail.com>
2025-09-18 21:12:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Bonjour 👋</h1>"
+    return "<h1>Bonjour je suis HAMZA ZAOUI👋</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
