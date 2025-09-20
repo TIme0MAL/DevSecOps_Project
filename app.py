45e043c3280dfb53bf1b31033fb63cc20d1aae7f
TIme0MAL <zaouihamza263@gmail.com>
2025-09-20 14:13:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 
+# Activer l’export des métriques
+metrics = PrometheusMetrics(app)
+
 @app.route("/")
 def home():
-    return "<h1>Bonjour je suis HAMZA ZAOUI👋</h1>"
+    return "<h1>Bonjour 👋</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
